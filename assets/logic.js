a40aa9b61a841f023324db05623b484a71eceb1c
DoxenNYC <jrdoxen@gmail.com>
2019-02-25 09:41:27 -0600

    reworked

--- assets/logic.js
+++ assets/logic.js
@@ -1,86 +1,75 @@
-var players = [
-    "Lebron James", "Kevin Durant", "Kawhi Leonard", "Kyrie Irving", "Paul George", "James Harden", "Stephen Curry", "Russell Westbrook", "Giannis Antetokounmpo", "Damian Lillard"
-  ];
-  
-  $(document).on("click", "button", function() {
-    var playerList = $(this).attr("data-person");
-  
-    var queryURL =
-      "https://api.giphy.com/v1/gifs/search?q=" + playerList + "&api_key=256Yui0vM0BJjz1GIjBE5mAwgtYieH5V&limit=10";
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response.data);
-  
-      var playerResult = response.data;
-  
-      $("#playerImg").empty();
-      for (var i = 0; i < playerResult.length; i++) {
-        var listGif = $("<div>");
-        listGif.attr({
-          class: "players"
-        });
+//still having issues making old gifs disappear once a new player is selected
+//maybe add a 10th player for an even 10
+//add a player button still has bugs
+var players = [ 
+	"Lebron James", "Kevin Durant", "Kyrie Irving", "Paul George", "Russell Westbrook", "Kawhi Leonard", "Stephen Curry", "James Harden", "Damian Lillard"
+];
 
-        var listRating = playerResult[i].rating;
-        var playerInfo = $("<p>").text("Rating: " + listRating);
-  
-        var playerGif = $(
-          "<img src='" + response.data[i].images.fixed_height_still.url + "'>"
-        );
-        playerGif.attr({
-          "data-state": "still",
-          "data-still": response.data[i].images.fixed_height_still.url,
-          "data-animate": response.data[i].images.fixed_height.url
-        });
-  
-        listGif.prepend(playerInfo);
-        
-        listGif.prepend(playerGif);
-  
-        $("#playerImg").prepend(listGif);
-      }
-    });
-  });
-  
-  function newPlayerButton() {
-    $("#playerButtons").empty();
-    $("#playerImg").empty();
-  
-    for (i = 0; i < players.length; i++) {
-      var playerBtn = $("<button>").text(players[i]);
-  
-      playerBtn.attr("data-person", players[i]);
-  
-      $("#playerButtons").append(playerBtn);
-    }
-  }
-  
-  $(document).on("click", "img", function() {
+for(var i = 0; i < players.length; i++) {
+	var button = $("<button>").text(players[i]);
+	button.attr("data-player", players[i]);
+	button.addClass("player-button");
+	$("#button-group").append(button);
+}
+
+$(document).on("click", ".player-button", function() {
+	var player = $(this).attr("data-player");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        player + "&api_key=256Yui0vM0BJjz1GIjBE5mAwgtYieH5V&limit=10";
+
+    $.ajax({
+    	url: queryURL,
+    	method: "GET"
+    }).done(function(response) {
+    	var outcome = response.data;
     
-    var state = $(this).attr("data-state");
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
+
+		var outcomeSection = $("<section class='outcome-container'>");
+
+    	for(var i = 0; i < outcome.length; i++) {
+    		var oneOutcome = $("<div class='outcome-container'>");
+    		
+    		var rating = outcome[i].rating;
+
+    		var p = $("<p>").text("Rating: " + rating);
+
+    		var playerImg = $("<img class='outcome'>");
+    		playerImg.attr("src", outcome[i].images.fixed_height_still.url);
+    		playerImg.attr("data-state", "still");
+    		playerImg.attr("data-still", outcome[i].images.fixed_height_still.url);
+    		playerImg.attr("data-animate", outcome[i].images.fixed_height.url);
+
+    		oneOutcome.prepend(playerImg);
+    		oneOutcome.prepend(p);
+
+    		outcomeSection.prepend(oneOutcome);
+    	}
+
+    	$("#players-group").prepend(outcomeSection);
+    });
+});
+
+$("#add-player-button").on("click", function(e) {
+	e.preventDefault();
+	if($("#new-player-input").val() !== "" && alreadyExist === false) {
+		var newPlayer = $("#new-player-input").val().toLowerCase();
+		players.push(newPlayer);
+		var button = $("<button>").text(newPlayer);
+		button.attr("data-player", newPlayer);
+		button.addClass("payer-button");
+		$("#button-group").append(button);
+	}
+	$("#new-player-input").val("");
+});
+
+$(document).on("click", ".outcome", function() {
+	var state = $(this).attr("data-state");
+
+	if(state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     }
-  });
-  
-  $("#submit").on("click", function(event) {
-    event.preventDefault();
-  
-    var addPlayer = $("#player-input")
-      .val()
-      .trim();
-  
-    players.push(addPlayer);
-    var name = $("<p>");
-    name.text(addPlayer);
-  
-    newPlayerButton();
-  });
-  
-  newPlayerButton();
+});
